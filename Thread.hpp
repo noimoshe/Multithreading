cbbecc43555639a4498abc3662525bbf622e0404
Noi Moshe <58302742+noimoshe@users.noreply.github.com>
2021-01-21 13:34:43 +0200

    Update Thread.hpp

--- Thread.hpp
+++ Thread.hpp
@@ -31,7 +31,7 @@ public:
         return this->m_thread_id;
 	}
 protected:
-	/** Implement this method in your subclass with the code you want your thread to run. */
+	
     virtual void thread_workload() = 0;
     uint m_thread_id; // A number from 0 -> Number of threads initialized, providing a simple numbering for you to use
 
