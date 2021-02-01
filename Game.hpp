5da04bb680c63f8307012f6ca45f2fd4ae3079b4
Noi Moshe <58302742+noimoshe@users.noreply.github.com>
2021-02-01 15:56:04 +0200

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -86,7 +86,6 @@ protected: // All members here are protected, instead of private for testing pur
 	uint m_gen_num; 			 // The number of generations to run
 	uint m_thread_num; 			 // Effective number of threads = min(thread_num, field_height)
 	vector<double> m_tile_hist; 	 // Shared Timing history for tiles: First m_gen_num cells are the calculation durations for tiles in generation 1 and so on.
-							   	 // Note: In your implementation, all m_thread_num threads must write to this structure. 
 	vector<double> m_gen_hist;  	 // Timing history for generations: x=m_gen_hist[t] iff generation t was calculated in x microseconds
 	vector<Thread*> m_threadpool; // A storage container for your threads. This acts as the threadpool.
 
